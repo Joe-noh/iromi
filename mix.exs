f59d8eeb01baa44caa875aa02c48cb57ab720546
Joe-noh <goflb.jh@gmail.com>
2016-08-31 01:30:47 +0900

    initial commit

---
+++ mix.exs
@@ -0,0 +1,15 @@
+defmodule Iromi.Mixfile do
+  use Mix.Project
+
+  def project do
+    [apps_path: "apps",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  defp deps do
+    []
+  end
+end
+
