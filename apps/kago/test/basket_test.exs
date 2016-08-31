f5e55444ed266e9e11811ba327a13766880144f6
Joe-noh <honzawa.j@pepabo.com>
2016-08-31 14:38:54 +0900

    add a missing test
    Kago.basket_content/1 returns the content

--- apps/kago/test/basket_test.exs
+++ apps/kago/test/basket_test.exs
@@ -2,7 +2,7 @@ defmodule BasketTest do
   use ExUnit.Case
   doctest Kago
 
-  test "worker can hold a basket" do
+  test "basket can hold items and calculate total price" do
     {:ok, basket} = Kago.new_basket
 
     Kago.add_item(basket, %Kago.Item{price: 500})
@@ -12,5 +12,17 @@ defmodule BasketTest do
     Kago.add_item(basket, %Kago.Item{price: 300})
 
     assert Kago.total_price(basket) == {:ok, 800}
+  end
+
+  test "basket can hold items and show its content" do
+    {:ok, basket} = Kago.new_basket
+
+    Kago.add_item(basket, plate = %Kago.Item{name: "plate", price: 300})
+    Kago.add_item(basket, spoon = %Kago.Item{name: "spoon", price: 100})
+
+    {:ok, content} = Kago.basket_content(basket)
+
+    assert plate in content.items
+    assert spoon in content.items
   end
 end
