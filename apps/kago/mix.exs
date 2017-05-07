76347bff019faef9405992f170af5462afa8313b
Joe-noh <goflb.jh@gmail.com>
2017-05-07 19:36:53 +0900

    might not required distillery

--- apps/kago/mix.exs
+++ apps/kago/mix.exs
@@ -35,9 +35,7 @@ defmodule Kago.Mixfile do
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
 
-     {:gproc, "~> 0.6"},
-
-     {:distillery, "~> 1.0"}]
+     {:gproc, "~> 0.6"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
